b620ba20d51bcef743222fb0b2040782dc849f15
unknown <dwaynezammit1999@gmail.com>
2024-09-12 17:05:17 +0200

    modified cdk deploy settings

--- wordpress-cdk-app/app.py
+++ wordpress-cdk-app/app.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 import os
-
 import aws_cdk as cdk
-
 from wordpress_cdk_app.wordpress_cdk_app_stack import WordpressCdkAppStack
 
+account = os.getenv('CDK_DEFAULT_ACCOUNT')
+region = os.getenv('CDK_DEFAULT_REGION', 'eu-west-1')  # Default to 'eu-west-1' if not set
+
+if not account:
+    raise ValueError("Environment variable 'CDK_DEFAULT_ACCOUNT' is not set.")
 
 app = cdk.App()
 WordpressCdkAppStack(app, "WordpressCdkAppStack",
-    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region='eu-west-1'),
-    )
+    env=cdk.Environment(account=account, region=region),
+)
 
 app.synth()
