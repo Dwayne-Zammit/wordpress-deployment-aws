1799570403ff350306588ad9bb991a55554a9842
unknown <dwaynezammit1999@gmail.com>
2024-09-12 17:15:35 +0200

    modified cdk deploy settings

--- wordpress-cdk-app/app.py
+++ wordpress-cdk-app/app.py
@@ -10,6 +10,7 @@ if not account:
     raise ValueError("Environment variable 'CDK_DEFAULT_ACCOUNT' is not set.")
 
 app = cdk.App()
+print(f"Deploying app to region {region}")
 WordpressCdkAppStack(app, "WordpressCdkAppStack",
     env=cdk.Environment(account=account, region=region),
 )
